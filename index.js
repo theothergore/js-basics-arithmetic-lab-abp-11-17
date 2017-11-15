6e85024e112739fa5e848977454116b95d020b22
Fly J Gore <jennifer@theothergore.com>
2017-11-15 16:18:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ const newID = 1000000000 + oldID;
 const ageIsValid = Number.isInteger(currentAge);
 const randomNumber = Math.random();
 const randomInteger = Math.floor(randomNumber);
-
+const randomUserID = Math.max(1000000001, 1000000020);
